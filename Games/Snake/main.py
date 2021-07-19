8c9f5e91efe298aeae41839549126a807168223f
Chet Jones <chetjones@pop-os.localdomain>
2021-07-18 22:52:53 -0500

    eat food functionality

--- Games/Snake/main.py
+++ Games/Snake/main.py
@@ -6,6 +6,7 @@ class SNAKE:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
         self.direction = Vector2(1, 0)
+        self.new_block = False
 
     def draw_snake(self):
         for block in self.body:
@@ -15,16 +16,23 @@ class SNAKE:
             pygame.draw.rect(screen, (0, 0, 0), block_rect)
 
     def move_snake(self):
-        body_copy = self.body[:-1]
-        body_copy.insert(0, body_copy[0] + self.direction)
-        self.body = body_copy[:]
+        if self.new_block == True:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+
+    def add_block(self):
+        self.new_block = True
 
 
 class FOOD:
     def __init__(self):
-        self.x = random.randint(0, cell_number - 1)
-        self.y = random.randint(0, cell_number - 1)
-        self.pos = Vector2(self.x, self.y)
+        self.randomize()
 
     def draw_food(self):
         food_rect = pygame.Rect(
@@ -33,7 +41,32 @@ class FOOD:
             cell_size,
             cell_size,
         )
+
         pygame.draw.rect(screen, (126, 166, 114), food_rect)
+
+    def randomize(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+
+
+class MAIN:
+    def __init__(self):
+        self.snake = SNAKE()
+        self.food = FOOD()
+
+    def update(self):
+        self.snake.move_snake()
+        self.check_collision()
+
+    def draw_elements(self):
+        self.food.draw_food()
+        self.snake.draw_snake()
+
+    def check_collision(self):
+        if self.food.pos == self.snake.body[0]:
+            self.food.randomize()
+            self.snake.add_block()
 
 
 pygame.init()
@@ -42,8 +75,7 @@ cell_number = 15
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
 
-food = FOOD()
-snake = SNAKE()
+main = MAIN()
 
 SREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SREEN_UPDATE, 250)
@@ -55,19 +87,18 @@ while True:
             pygame.quit()
             sys.exit()
         if event.type == SREEN_UPDATE:
-            snake.move_snake()
+            main.update()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                snake.direction = Vector2(0, -1)
+                main.snake.direction = Vector2(0, -1)
             if event.key == pygame.K_DOWN:
-                snake.direction = Vector2(0, 1)
+                main.snake.direction = Vector2(0, 1)
             if event.key == pygame.K_RIGHT:
-                snake.direction = Vector2(1, 0)
+                main.snake.direction = Vector2(1, 0)
             if event.key == pygame.K_LEFT:
-                snake.direction = Vector2(-1, 0)
+                main.snake.direction = Vector2(-1, 0)
 
     screen.fill((175, 215, 70))
-    food.draw_food()
-    snake.draw_snake()
+    main.draw_elements()
     pygame.display.update()
     clock.tick(60)
